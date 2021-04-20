3e6470e20180bf174d3a804faf5d67c020792cb2
Merlino79 <alessandro.ciancio@hotmail.com>
2021-04-20 22:42:33 +0200

    copia ed incolla della funzone per ir ritorno...

--- asset/js/script.js
+++ asset/js/script.js
@@ -41,6 +41,31 @@ $(document).ready(function() {
 
     function prevImage() {
         console.log("prev");
+        var activeImg = $('.box-img img.visible');
+        console.log(activeImg);
+
+        var activeCircle = $(' li.active');
+        console.log(activeCircle);
+        // activeCircle.removeClass('active');
+
+        activeImg.removeClass('visible');
+        activeCircle.removeClass('active')
+            //console.log(activeImg.next());
+
+        if (activeImg.hasClass('last')) {
+            //aggiungo active a first
+            $('.box-img img.first').addClass('visible');
+            $('li.first').addClass('active');
+
+        } else {
+            activeImg.next('').addClass('visible')
+            activeCircle.next('').addClass('active')
+        }
+
+
+        activeImg.next('img').addClass('visible');
+
+
     }
 
 
